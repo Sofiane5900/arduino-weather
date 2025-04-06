cef59684dbc6de62d4e295cccc201c0deba2dc77
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-04-06 14:54:27 +0200

    feat: adding temp & humidity basic support

--- weather/weather.ino
+++ weather/weather.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
 
+// pins analog in
+#define DHTPIN A0 
+//définition du capteur dht11 
+#define DHTTYPE DHT11
+// création d'un objet dht avec son pins et son type
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println("DHT11 test...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2000);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Erreur.");
+  } else {
+    Serial.print("Humidité: ");
+    Serial.print(h);
+    Serial.print(" % | Temperature: ");
+    Serial.print(t);
+    Serial.println(" °C");
+  }
 }
